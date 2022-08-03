7452eb3e3c38a9b383e2124cc202eb3fac5d27c6
Otavio Cordeiro Lopes <otavio.cordeiro.ext@brf.com>
2022-08-03 17:26:38 -0300

    Ajuste no response

--- TestBackEndApi.Domain/Queries/Cep/Get/GetCepQueryResponse.cs
+++ TestBackEndApi.Domain/Queries/Cep/Get/GetCepQueryResponse.cs
@@ -14,16 +14,6 @@ namespace TestBackEndApi.Domain.Queries.Cep.Get
 
         public string Localidade { get; set; }
 
-        public string Uf { get; set; }
-
-        public string Ibge { get; set; }
-
-        public string Gia { get; set; }
-
-        public string Ddd { get; set; }
-
-        public string Siafi { get; set; }
-
         public List<MensagemQueryResponse> Mensagens { get; set; }
     }
 
